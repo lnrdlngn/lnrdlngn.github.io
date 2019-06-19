e35544365c514aea861e4a62dc83f32f2a06b998
lnrdlngn <51335899+lnrdlngn@users.noreply.github.com>
2019-06-19 10:01:38 +0200

    moved worker include

--- soundboard-app/scripts/scripts.js
+++ soundboard-app/scripts/scripts.js
@@ -1,20 +1,30 @@
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('app/service-worker.js');
+    });
+}
+
+
+
 $('document').ready(function () {
 
-  var objWarte = document.createElement("audio");
-  objWarte.src = "media/audio/warte.mp3";
-  objWarte.volume = 1;
-  objWarte.autoPlay = false;
-  objWarte.preLoad = true;
-  objWarte.controls = false;
+    var objWarte = document.createElement("audio");
+    objWarte.src = "media/audio/warte.mp3";
+    objWarte.volume = 1;
+    objWarte.autoPlay = false;
+    objWarte.preLoad = true;
+    objWarte.controls = false;
 
-  $("#playWarte").click(function() {
-    if ($("#playWarte").hasClass('playing')) {
-      objWarte.pause();
-      $("#playWarte").removeClass('playing');
-    } else {
-      objWarte.play();
-      $("#playWarte").addClass('playing');
-    }
-  });
+    $("#playWarte").click(function() {
+        if ($("#playWarte").hasClass('playing')) {
+            objWarte.pause();
+            $("#playWarte").removeClass('playing');
+        } else {
+            objWarte.play();
+            $("#playWarte").addClass('playing');
+        }
+    });
 
 });

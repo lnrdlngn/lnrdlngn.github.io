7d6cd01c9e3bd9e573b5cd3e3cd80cb169d8809b
lnrdlngn <51335899+lnrdlngn@users.noreply.github.com>
2019-06-21 11:59:15 +0200

    delete caches

--- soundboard-app/service-worker.js
+++ soundboard-app/service-worker.js
@@ -1,5 +1,3 @@
-console.log('Hello from service-worker.js');
-
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
 if (workbox) {
@@ -13,24 +11,7 @@ workbox.setConfig({ debug: true });
 
 
 
-workbox.routing.registerRoute(
-    // Cache HTML files
-    new RegExp('.+\\.html$'),
-    new workbox.strategies.NetworkFirst()
-);
-
-workbox.routing.registerRoute(
-    // Cache JS files
-    new RegExp('.+\\.js$'),
-    new workbox.strategies.NetworkFirst()
-);
-
-workbox.routing.registerRoute(
-    // Cache CSS files
-    new RegExp('.+\\.css$'),
-    new workbox.strategies.NetworkFirst()
-);
-
+// Cache all files
 workbox.routing.registerRoute(
   new RegExp('/'),
   new workbox.strategies.NetworkFirst()
